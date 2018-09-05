52569ad406ed6090a5e8386aa176f23ff300edc6
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-05 12:54:49 -0400

    commit

--- SimpleNumberFormatter.java
+++ SimpleNumberFormatter.java
@@ -1,20 +1,45 @@
 package com.sangjun.java_practice;
+//DecimalFormatting
+
+import org.junit.Test;
 
 import java.text.DecimalFormat;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 public class SimpleNumberFormatter {
     private static DecimalFormat decimalFormat = new DecimalFormat("###,###,###.###");
+
+
+
+    public static void setDecimalFormat(DecimalFormat decimalFormat) {
+        SimpleNumberFormatter.decimalFormat = decimalFormat;
+    }
 
     public String format(double number) {
         String result = decimalFormat.format(number);
         return result;
     }
 
-    public void setFormatter(DecimalFormat other) {
-        decimalFormat = other;
-    }
 
     public DecimalFormat getFormatter() {
         return decimalFormat;
+    }
+
+    @Test
+    public void test1(){
+        double test = 123456.789;
+        System.out.println(format(test));
+        assertTrue(test == 123456.789);
+
+
+        DecimalFormat newFormat = new DecimalFormat("000,000,000.000");
+        setDecimalFormat(newFormat);
+        System.out.println(format(test));
+        assertEquals(format(test), "000,123,456.789");
+        assertNotEquals(test, "000,123,456.789");
+
     }
 }
