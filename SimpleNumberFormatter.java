c74ae4d835bc92550db1b94e76c382b5df6d8685
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-04 23:23:16 -0400

    commit

--- SimpleNumberFormatter.java
+++ SimpleNumberFormatter.java
@@ -1,4 +1,20 @@
 package com.sangjun.java_practice;
 
+import java.text.DecimalFormat;
+
 public class SimpleNumberFormatter {
+    private static DecimalFormat decimalFormat = new DecimalFormat("###,###,###.###");
+
+    public String format(double number) {
+        String result = decimalFormat.format(number);
+        return result;
+    }
+
+    public void setFormatter(DecimalFormat other) {
+        decimalFormat = other;
+    }
+
+    public DecimalFormat getFormatter() {
+        return decimalFormat;
+    }
 }
