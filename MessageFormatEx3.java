6c94cf7c901bc5f0f259e8c34d14920deb739cef
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-19 20:52:53 -0400

    commit

--- MessageFormatEx3.java
+++ MessageFormatEx3.java
@@ -1,9 +1,29 @@
 package com.sangjun.java_practice;
 
+import java.text.MessageFormat;
+import java.text.ParseException;
+
 /**
  * Created by bobsang89@gmail.com on 2018-09-19
  * Project: java-practice
  * Github : http://github.com/SangJun-GitHub
  */
-public class MessageFormateEx3 {
+public class MessageFormatEx3 {
+    public static void main(String[] args) throws Exception {
+        String[] data = {
+                "INSERT INTO CUST_INFO VALUES ('Sangjun','000-000-0000','30','08-30');",
+                "INSERT INTO CUST_INFO VALUES ('Park','111-111-1111','31','01-01');"
+        };
+
+        String pattern = "INSERT INTO CUST_INFO VALUES ({0},{1},{2},{3});";
+        MessageFormat messageFormat = new MessageFormat(pattern);
+
+        for(int i =0; i < data.length; i++){
+            Object[] objs = messageFormat.parse(data[i]);
+            for(int j =0; j< objs.length; j++){
+                System.out.print(objs[j] + ",");
+            }
+            System.out.println();
+        }
+    }
 }
--- MessageFormatEx4.java
+++ MessageFormatEx4.java
@@ -1,9 +1,28 @@
 package com.sangjun.java_practice;
 
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.Scanner;
+
 /**
  * Created by bobsang89@gmail.com on 2018-09-19
  * Project: java-practice
  * Github : http://github.com/SangJun-GitHub
  */
-public class MemssageFormatEx4 {
+public class MessageFormatEx4 {
+    public static void main(String[] args) throws Exception{
+        String tableName = "CUT_INFO";
+        String fileName = "./data4.txt";
+        String msg = "INSERT INTO "+ tableName + " VALUES ({0}, {1}, {2}, {3});";
+        Scanner s = new Scanner(new File(fileName));
+
+        String pattern = "{0}, {1}, {2}, {3}";
+        MessageFormat messageFormat = new MessageFormat(pattern);
+
+        while (s.hasNextLine()){
+            String line = s.nextLine();
+            Object[] objs = messageFormat.parse(line);
+            System.out.println(MessageFormat.format(msg, objs));
+        }
+        s.close();    }
 }
