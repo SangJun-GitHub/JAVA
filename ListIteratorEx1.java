ffeff090d44acf28a435892c1adaa8bbb0efaa1f
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-25 19:18:41 -0400

    commit

--- ListIteratorEx1.java
+++ ListIteratorEx1.java
@@ -21,6 +21,15 @@ public class ListIteratorEx1 {
 //        Iterator is one way. so, we can use iterator when iterator reach last element
 //        but, ListIterator is two way. so ListIterator has more freedom than iterator
 //        but, before move to element we have to call hasNext() or hasPrevious() for checking possibility to move
+//        void add(Object object) - add object to collection
+//        boolean hasNext() - check next elements, if exist return true
+//        boolean hasPrevious() - check previous elements, if exist return true
+//        Object next() - return next object
+//        Object previous() - return previous object
+//        int nextIndex() return next element's index
+//        int previousIndex() return previous element's index
+//        void remove() - remove element, but after call next() or previous()
+//        set(Object object) - set object with read element, but after next() or previous()
 
         ListIterator iterator = list.listIterator();
 
