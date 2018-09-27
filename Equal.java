00a649f079604949f257ddeea07cdc9c1e846276
SangJun-HitHub <sjpark24@myseneca.ca>
2018-09-26 21:22:23 -0400

    commit

--- Equal.java
+++ Equal.java
@@ -6,4 +6,12 @@ package com.sangjun.java_practice;
  * Github : http://github.com/SangJun-GitHub
  */
 public class Equal {
+    public static void main(String[] args){
+        System.out.println("new Integer(1).equals(new Integer(1))");
+        System.out.println(new Integer(1).equals(new Integer(1))); // 동등성
+        System.out.println("1 == new Integer(1)");
+        System.out.println(1 == new Integer(1)); //언박싱 뉴인티저에서 1을 꺼내서 비교
+        System.out.println("new Integer(1) == new Integer(1)");
+        System.out.println(new Integer(1) == new Integer(1));// 동일성 즉 주소값일지
+    }
 }
