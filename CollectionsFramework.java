8d777599d56eee51b98d5e4ef85a3a08ab08f2c1
SangJun-HitHub <sjpark24@myseneca.ca>
2018-10-03 21:14:08 -0400

    commit

--- CollectionsFramework.java
+++ CollectionsFramework.java
@@ -22,31 +22,34 @@ public class CollectionsFramework {
         //Collection interface
         //boolean add(Object o) - 지정된 Objectd의 객체들을 Collection에 추가
         //boolean add(Collection c) - 지정된 Collection의 객체들을 Collection에 추가
-
         //void clear() - Collection의 모든 데이터 삭제
-
         //boolean contains(Object o)  - 지정된 Object의 객체가 Collection에 있는지 확인
         //boolean contains(Collection c) - 지정된 Collection의 객체가가 Collection에 있는지 확인
-
         //boolean equals(Object o) - 동일한 Collection인지 비교
-
         //int hashCode() - Colleaction의 hash Code 반환
-
         //boolean isEmpty() - Collection이 비어있는지 확인
-
         //Iterator iterator() - Collection의 Iterator를 반환
-
         //boolean remove(Object o) - 지정된 객체 삭제
-
         //boolean removeAll(Collection c) - 지정된 Collection에 포함된 모든 객체를 삭제
-
         //boolean retainAll(Collection c) - 지정된 Collection에 포함된 객체만 남기고 다른객체들을 Collection에서 삭제
         //                                  Collection에 변화가 있으면 true 없으면 false
-
         //int size() - Collection에 저장된 객체의 개수를 반환
-
         //Object[] toArray() - Collection에 저장된 객체를 객체배열(Object[])로 반환
-
         //Object[] toArray(Object[] a) - 지정된 배열에 Collection의 객체를 저장해서 반환
+        
+
+        //List interface- Vector-Stack, ArrayList, LinkedList
+        //중복을 허용, 저장순서가 유지
+        //void add(int index, Object element) - 지정된 index에 객체를 추가
+        //boolean addAll(int index, Collection c)  - 지정된 index에 Colltion에 포함된 객체를 추가
+        //Object get(int index) - 지정된 위치의 객체를 반환
+        //int indexOf(Object o) - 지정된 객체의 index를 반환 (정방향)
+        //int lastIndexOf(Object o) - 지정된 객체의 index를 반환 (역방향)
+        //ListIterator listIterator()  - List의 객체에 접근할 수있는 ListIterator반환
+        //ListIterator listIterator(int index) - List의 객체에 접근할 수있는 ListIterator반환
+        //Object remove(int index) - 지정된 index의 객체를 삭제하고 삭제된 객체를 반환
+        //Object set(int index, Object element) - 지정된 index에 객체를 저장
+        //void sort(Comparator c) - Comparator를 통하여 List 정렬
+        //List subList(int formIndex, int toIndex) - fromIndex부터 toIndex까지의 객체를 반환
     }
 }
