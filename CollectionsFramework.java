790e2f0d230f0b987b8553398e9ab8f019e4ef3a
SangJun-HitHub <sjpark24@myseneca.ca>
2018-10-03 21:30:54 -0400

    commit

--- CollectionsFramework.java
+++ CollectionsFramework.java
@@ -36,7 +36,7 @@ public class CollectionsFramework {
         //int size() - Collection에 저장된 객체의 개수를 반환
         //Object[] toArray() - Collection에 저장된 객체를 객체배열(Object[])로 반환
         //Object[] toArray(Object[] a) - 지정된 배열에 Collection의 객체를 저장해서 반환
-        
+
 
         //List interface- Vector-Stack, ArrayList, LinkedList
         //중복을 허용, 저장순서가 유지
@@ -51,5 +51,30 @@ public class CollectionsFramework {
         //Object set(int index, Object element) - 지정된 index에 객체를 저장
         //void sort(Comparator c) - Comparator를 통하여 List 정렬
         //List subList(int formIndex, int toIndex) - fromIndex부터 toIndex까지의 객체를 반환
+
+
+        //Set interface - HashSet, SortedSet-TreeSet
+        //중복을 허용 안함, 저장순서 유지 안함
+
+
+        //Map interface - Hashtable, HashMap-LinkedHashMap, SortedMap-TreeMap
+        //Key + Value = pair (Key 중복 허용 안함, Value 중복 허용) *중복키를 저장한다면 기존의 저장된 값이 지워지고 마지막에 저장된 값이 저장됨
+        //void clear() - Map의 모든 객체 삭제
+        //boolean containsKey(Object key) - 지정된 key객체와 일치하는 Map의 key가 있는지 확인
+        //boolean containsValue(Object value) -지정된 value객체와 일치하는 Map의 value객체가 있는지 확인
+        //Set entrySet() - Map에 저장된 pair를 Map.Entry타입의 객체로 저장한 Set으로 반환
+        //boolean equals(Object o) - 동일한 Map인지 비교
+        //Object get(Object key) - key와 대응하는 value 반환
+        //int hasCode() - hachCode 반환
+        //boolean inEmpty() - Map이 비어있는지 확인
+        //Set keySet() - Map에 지정된 모든 key 반환
+        //Object put(Object key, Object value) - value를 key에 연결하여 저장
+        //void putAll(Map t) - 모든 pair 추가
+        //Object remove(Object key) - key와 일치하는 pair 삭제
+        //int size() - pair 개수 반환
+        //Collection values() - 모든 value 반환
+
+
+
     }
 }
