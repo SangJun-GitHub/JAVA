687723da402f2ac0d1a213355a1098d1b9fa4da8
SangJun-HitHub <sjpark24@myseneca.ca>
2018-11-05 21:53:30 -0500

    commit

--- VarargsOverloadingTest.java
+++ VarargsOverloadingTest.java
@@ -6,4 +6,22 @@ package com.sangjun.java_practice;
  * Github : http://github.com/SangJun-GitHub
  */
 public class VarargsOverloadingTest {
+    static String concatenate(String delimiter, String... args){
+        String result = "";
+        for(String str: args)
+            result += str+delimiter;
+
+        return result;
+    }
+
+    static public void main(String[] args){
+        String[] strArr = {"111","222","333"};
+
+        System.out.println(concatenate("", strArr));
+        System.out.println(concatenate("-", strArr));
+        System.out.println(concatenate(",", new String[]{"100","200","300"}));
+        System.out.println("["+concatenate(",",new String[0])+"]");
+        System.out.println("["+concatenate(",")+"]");
+    }
 }
+
