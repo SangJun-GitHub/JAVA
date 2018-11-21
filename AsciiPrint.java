31764553b1ef2252af389dfe6211a27f3b4b7075
SangJun-HitHub <sjpark24@myseneca.ca>
2018-11-21 17:57:23 -0500

    commit

---
+++ AsciiPrint.java
@@ -0,0 +1,15 @@
+package com.sangjun.java_practice;
+
+/**
+ * Created by bobsang89@gmail.com on 2018-11-21
+ * Project: java-practice
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class AsciiPrint {
+    public static void main(String[] args){
+        char ch = ' ';
+        for(int i =0; i<95;i++){
+            System.out.print(ch++);
+        }
+    }
+}
